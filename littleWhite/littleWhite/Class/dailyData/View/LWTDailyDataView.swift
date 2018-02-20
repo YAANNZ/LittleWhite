890236993cf71b25741fc2e9c4e00b73bd534ab9
ZYYNN <zhuynchn@163.com>
2018-02-20 20:24:34 +0800

    dailydataview

---
+++ littleWhite/littleWhite/Class/dailyData/View/LWTDailyDataView.swift
@@ -0,0 +1,24 @@
+//
+//  LWTDailyDataView.swift
+//  littleWhite
+//
+//  Created by ZHUYN on 2018/2/20.
+//  Copyright © 2018年 zynabc. All rights reserved.
+//
+
+import UIKit
+
+class LWTDailyDataView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+}
+
